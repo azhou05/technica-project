fd17d721e1e387c7a56b762046bfaf754226aa68
Ivy Lu <ivylu21122@gmail.com>
2024-10-26 12:27:10 -0400

    added index.html

--- src/components/home.js
+++ src/components/home.js
@@ -1,1 +1,18 @@
 // page for login info
+import styles from "./home.scss"
+
+function About() {
+
+  return (
+    <div className='home-page'>
+      <h2>Home</h2>
+      <div className='home'>
+        <p>
+            This is a home page
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About;
